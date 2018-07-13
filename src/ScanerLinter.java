ad6442fc6bc6ceb46cad82e757035f717647f266
Suzanne Richman <suzanne@favoredfortune.com>
2018-07-13 12:49:08 -0700

    working on linter problem but getting some weird variable format errors. not done

---
+++ src/ScanerLinter.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ScanerLinter {
+    public static void main(String[] args) {
+
+        //set file name to scan
+        String filename = "gates.js";
+
+        //tell program to create new instance of filename to evaluate
+        // as a file using scanner method
+        File file   = new File(filename);
+
+        //invoke method to scan file, in this case gates.js
+         jsLinter(file);
+    }
+
+    public static void jsLinter(String file){
+        int line = 0;
+        String lineHolder = "";
+
+        try{
+            Scanner scannerJS = new Scanner(file);
+            while(scannerJS.hasNextLine()){
+
+                //store content/token of next for evaluation with this variable
+                lineHolder = scannerJS.nextLine();
+                line++;
+                if ((!lineHolder.contains("{") && !lineHolder.contains("}") && !lineHolder.contains("if") &&
+                        !lineHolder.contains("else") && !lineHolder.isEmpty())){
+                    System.out.println("Line " + line + ": Missing semicolon");
+                }
+            }
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+        }
+    }
+}
